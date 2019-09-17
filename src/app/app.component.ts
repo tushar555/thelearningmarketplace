7ff1d08c6d14990d29609469b67754e779f40897
Tushar Saindane <tushar.saindane2@bcone.com>
2019-09-17 17:05:21 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'LearningMarketPlace';
+  title = "The Learning Market Place!";
+  userEmail: any;
+  userName: any;
+  showUsers: any = false;
+  showSuccessMessage: any = false;
+  loginForm: any;
+  btnTitle: any = "Show Users";
+  constructor(private _fb: FormBuilder) {
+    this.loginForm = this._fb.group({
+      userEmail: ["", Validators.compose([Validators.required])],
+      userName: ["", Validators.compose([Validators.required])]
+    });
+  }
+
+  onSubmit() {
+    this.showSuccessMessage = true;
+
+    this.userEmail = [];
+    this.userName = [];
+
+    this.userEmail = JSON.parse(localStorage.getItem("userEmail"));
+
+    //  this.userName = JSON.parse(localStorage.getItem("userName"));
+    this.localStorageConditions(
+      this.userEmail,
+      this.loginForm.value.userEmail,
+      this.loginForm.value.userName
+    );
+
+    setTimeout(() => {
+      this.showSuccessMessage = false;
+    }, 2000);
+  }
+
+  localStorageConditions(arrayName, email, name) {
+    if (arrayName !== null) {
+      arrayName.push({ email: email, full_name: name });
+    } else {
+      this.userEmail = [];
+
+      this.userEmail[0] = { email: email, full_name: name };
+    }
+    localStorage.setItem("userEmail", JSON.stringify(this.userEmail));
+  }
+  viewUsers() {
+    this.showUsers = !this.showUsers;
+    this.btnTitle = this.showUsers ? "Hide Users" : "Show Users";
+
+    this.userEmail = JSON.parse(localStorage.getItem("userEmail"));
+  }
 }
